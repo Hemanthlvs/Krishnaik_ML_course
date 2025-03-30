4c50c0a986d07a42542ee3f4615e06ca70a93a92
Lankayagari Venkata Sai Hemanth <Lankayagari.Venkata.Sai.Hemanth@gds.ey.com>
2025-03-30 22:18:32 +0530

    git required files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Hemanthlvs",
+    author_email="lvshemanth@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
