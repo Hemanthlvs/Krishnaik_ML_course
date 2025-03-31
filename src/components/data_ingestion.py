1a0d6d34aba905801082e4a95d2e87e4f4955516
Lankayagari Venkata Sai Hemanth <Lankayagari.Venkata.Sai.Hemanth@gds.ey.com>
2025-03-31 17:52:22 +0530

    data ingestion script created

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import os
+from src.logger import logging
+from src.exception import CustomException
+
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+@dataclass
+class path_config:
+    raw_path = os.path.join('artifacts', 'raw_data.csv')
+    test_path = os.path.join('artifacts', 'test_data.csv')
+    train_path = os.path.join('artifacts', 'train_data.csv')
+
+class ingestion:
+    def __init__(self):
+        self.ingestion_paths = path_config()
+
+    def initiate_ingestion(self):
+        logging.info("Ingestion started...........")
+
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info("Dataframe Created")
+            os.makedirs(os.path.dirname(self.ingestion_paths.raw_path),exist_ok=True)
+            logging.info("Artifacts folder created")
+            df.to_csv(self.ingestion_paths.raw_path)
+            logging.info("raw file created")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_paths.train_path)
+            logging.info("training file created")
+            test_set.to_csv(self.ingestion_paths.test_path)
+            logging.info("testing file created")
+
+            return (self.ingestion_paths.raw_path,
+                    self.ingestion_paths.train_path,
+                    self.ingestion_paths.test_path
+                    )
+        except Exception as e:
+            raise CustomException
+
+if __name__ == "__main__":
+    obj = ingestion()
+    obj.initiate_ingestion()
+
+
+
